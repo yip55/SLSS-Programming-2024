c401f1920de26e967fe9b2e8a0d525050715b508
odyssa yip <odyssayip@icloud.com>
2024-04-11 11:11:38 -0700

    added

--- notes-2-2-morefunctions.py
+++ notes-2-2-morefunctions.py
@@ -18,13 +18,13 @@ def stars(num_stars: int) -> str:
   
     return value
 
-def pyramid(base_width: int):
-    """prints a pyrimid of stars of given base width
-    params:
-        base_width - bottom row of stars 
-    """
-    for i in range(base_width):
-        print(stars(i+  1))
+# def pyramid(base_width: int):
+#     """prints a pyrimid of stars of given base width
+#     params:
+#         base_width - bottom row of stars 
+#     """
+#     for i in range(base_width):
+#         print(stars(i+  1))
 
 
 # multiply stings 
@@ -37,6 +37,6 @@ print(stars(100))
 print(stars(0))
 print(stars(-1))
 
-pyramid(1)
-pyramid(5)
-pyramid(20)
+# pyramid(1)
+# pyramid(5)
+# pyramid(20)
--- unit-mid2-activity.py
+++ unit-mid2-activity.py
@@ -2,3 +2,40 @@
 # Author: odyssa yip
 # april 9 2024
 
+def best_driver(driver):
+        
+    if driver.lower().strip("!.?, ") == "lewis hamilton":
+        return"CORRECT!"
+    if driver.lower().strip("!.?, ") == "max verstappen":
+        return"WRONG! Absolutely not"
+    if driver.lower().strip("!.?, ") == "kimi raikonnen":
+        return"okay, maybe"
+    if driver.lower().strip("!.?, ") == "sebastian vettel":
+        return"Yes"
+    if driver.lower().strip("!.?, ") == "ayrton senna":
+        return"of course"
+    else:
+        return"ummm, I don't think so"
+        
+
+    driver = input("who's the best F1 driver ever?")
+
+    result = best_driver(driver)
+    
+    return result
+
+# user_answer = input("who's the best F1 driver ever?").lower().strip("!.?, ")
+
+# while user_answer not in ["lewis hamilton", "max verstappen", "ayrton senna"]:
+#     user_answer = input("i don't think so?").lower().strip("!.?, ")
+
+# if user_answer in["lewis hamilton", "ayrton senna"]:
+#     print("AGREE")
+# if user_answer in ["max verstappen"]:
+#     print("ummm, I don't think so")
+user_answer = input("who's the best F1 driver ever?").lower().strip("!.?, ")
+
+while user_answer not in ["lewis hamilton", "max verstappen", "ayrton senna", "kimi raikonnen", "sebastian vettel"]:
+    user_answer = input("i don't think so? seriously, who's the best F1 driver ever? ").lower().strip("!.?, ")
+
+print(best_driver(user_answer))
