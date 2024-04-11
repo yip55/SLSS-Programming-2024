9e1a00ddc7ecf9b0e9a62eca1858c5e5938feb60
odyssa yip <odyssayip@icloud.com>
2024-04-11 11:37:10 -0700

    added

--- unit-mid2-activity.py
+++ unit-mid2-activity.py
@@ -18,21 +18,9 @@ def best_driver(driver):
         return"ummm, I don't think so"
         
 
-    driver = input("who's the best F1 driver ever?")
 
-    result = best_driver(driver)
-    
-    return result
 
-# user_answer = input("who's the best F1 driver ever?").lower().strip("!.?, ")
 
-# while user_answer not in ["lewis hamilton", "max verstappen", "ayrton senna"]:
-#     user_answer = input("i don't think so?").lower().strip("!.?, ")
-
-# if user_answer in["lewis hamilton", "ayrton senna"]:
-#     print("AGREE")
-# if user_answer in ["max verstappen"]:
-#     print("ummm, I don't think so")
 user_answer = input("who's the best F1 driver ever?").lower().strip("!.?, ")
 
 while user_answer not in ["lewis hamilton", "max verstappen", "ayrton senna", "kimi raikonnen", "sebastian vettel"]:
